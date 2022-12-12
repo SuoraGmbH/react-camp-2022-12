26266226fe908e601c540e51051e94a82179a279
Hans-Christian Otto <c.otto@suora.com>
2022-12-12 14:59:27 +0100

    add timeentryview

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import Welcome from "./Welcome";
-import { ComponentProps } from "react";
+import React, { ComponentProps } from "react";
+import TimeEntryView from "./components/TimeEntryView";
 
 // Example on how to get the type of props of a component
 // const welcomeProps: ComponentProps<typeof Welcome> = { city: "some city" };
 
 function App() {
   return (
-    <div>
+    <>
       <h1>Hallo React aus anderer Datei!</h1>
       <Welcome city="Düsseldorf" />
-    </div>
+      <TimeEntryView comment="React lernen" />
+      <TimeEntryView comment="React meistern" />
+    </>
   );
 }
 

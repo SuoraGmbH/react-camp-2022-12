241825e41df0297a44d4cfa038e467c2c398a481
Hans-Christian Otto <c.otto@suora.com>
2022-12-12 11:18:38 +0100

    fix name

--- typescript/keyof.ts
+++ typescript/keyof.ts
@@ -1,12 +1,14 @@
-const colorMap = {
+const colorMapping = {
   red: "#ff0000",
   blue: "#0000ff",
   green: "#00ff00",
   purple: "#123874",
 };
 
-export function colorNameToHexCode(colorName: keyof typeof colorMap): string {
-  return colorMap[colorName];
+export function colorNameToHexCode(
+  colorName: keyof typeof colorMapping
+): string {
+  return colorMapping[colorName];
 }
 
 colorNameToHexCode("purple");

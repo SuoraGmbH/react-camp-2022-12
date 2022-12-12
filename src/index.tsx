38888d2c7f3c48f40ff7b7995d73290106f4c2aa
Hans-Christian Otto <c.otto@suora.com>
2022-12-12 13:47:57 +0100

    component

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const element = <h1>Hallo Düsseldorf</h1>;
+function App() {
+  return <h1>Hallo React!</h1>;
+}
 
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));

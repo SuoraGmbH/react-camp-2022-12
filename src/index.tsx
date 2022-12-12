f1839c803e002f043feb40819d31234e9b02f7c3
Hans-Christian Otto <c.otto@suora.com>
2022-12-12 12:21:26 +0100

    first exercise

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const header = <h1>Hallo Düsseldorf</h1>;
+const element = <div>Düsseldorf ist schöner als Köln</div>;
+
+ReactDOM.render(header, document.getElementById("header"));
+ReactDOM.render(element, document.getElementById("main"));

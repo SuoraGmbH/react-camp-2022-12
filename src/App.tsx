aeb7e7db97aaa16aedaa943f98e8db2e857f0478
Hans-Christian Otto <c.otto@suora.com>
2022-12-13 14:58:00 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import Welcome from "./components/Welcome";
 import React, { ComponentProps } from "react";
-import TimeEntryView from "./components/TimeEntryView";
 import { TimeEntry } from "./domain/TimeEntry";
 import TimeEntryForm from "./components/TimeEntryForm";
-import Counter from "./components/Counter";
-import GithubRepoStats from "./components/GithubRepoStats";
 import TimeEntryList from "./components/TimeEntryList";
 
 // Example on how to get the type of props of a component

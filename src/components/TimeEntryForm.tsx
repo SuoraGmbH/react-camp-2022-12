ce1b0547bc8188a8f35b401038c2dd561c8fe9fa
Hans-Christian Otto <c.otto@suora.com>
2022-12-13 10:32:09 +0100

    Controlled Component example

--- src/components/TimeEntryForm.tsx
+++ src/components/TimeEntryForm.tsx
@@ -6,15 +6,16 @@ const TimeEntryForm: React.FunctionComponent = () => {
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
     console.log("Hello World");
     event.preventDefault();
+    setInputValue("");
   };
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    setInputValue(event.target.value);
+    setInputValue(event.target.value.toUpperCase().replace("A", "B"));
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input onChange={handleChange} />
+      <input onChange={handleChange} value={inputValue} />
       {inputValue}
       <button type="submit">Submit</button>
     </form>

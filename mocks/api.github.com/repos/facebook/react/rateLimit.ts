d29a6a94d236b9e79df8b7541c23b569a00474cc
Hans-Christian Otto <c.otto@suora.com>
2022-12-14 10:52:17 +0100

    disable delay

--- mocks/api.github.com/repos/facebook/react/rateLimit.ts
+++ mocks/api.github.com/repos/facebook/react/rateLimit.ts
@@ -5,7 +5,7 @@ export const githubRepoFacebookReactRateLimitMock = rest.get(
   (req, res, context) => {
     return res(
       context.status(403),
-      context.delay(5000),
+      // context.delay(5000),
       context.json({
         message:
           "API rate limit exceeded for 93.245.109.13. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)",
